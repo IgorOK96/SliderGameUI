7acc8c5de1d7cb1f3d84256f9d3b2d24c19946b9
IgorOK96 <IgorOK96@users.noreply.github.com>
2024-10-17 18:11:26 +0200

    Initial Commit

---
+++ SliderGameUI/SliderGameUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameUIApp.swift
+//  SliderGameUI
+//
+//  Created by user246073 on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
